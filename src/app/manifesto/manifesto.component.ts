733b5ecc9267edcf936e1430ba0a933e52b18264
Nate H <nathan.hertner@gmail.com>
2024-05-27 19:28:18 -0400

    .

--- src/app/manifesto/manifesto.component.ts
+++ src/app/manifesto/manifesto.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
+import { NgxExtendedPdfViewerComponent, NgxExtendedPdfViewerService, NgxExtendedPdfViewerModule } from 'ngx-extended-pdf-viewer';
 
 @Component({
   selector: 'app-manifesto',
   standalone: true,
-  imports: [],
+  imports: [NgxExtendedPdfViewerModule],
   templateUrl: './manifesto.component.html',
   styleUrl: './manifesto.component.css'
 })
 export class ManifestoComponent {
-
+  // src = '/assets/documents/B3FL_Manifesto.pdf';
+  src = 'https://www.clickdimensions.com/links/TestPDFfile.pdf';
 }
